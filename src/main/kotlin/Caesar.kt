7fb3bf6919b482e27e0d061b8cfbe1c58cbd2ddc
manasa.ch <manasa.c@marketsimplified.com>
2023-07-12 14:35:40 +0530

    commit

--- src/main/kotlin/Caesar.kt
+++ src/main/kotlin/Caesar.kt
@@ -3,15 +3,37 @@ import java.util.Scanner
 fun main() {
     val scanner=Scanner(System.`in`)
     val str= readLine()
-    val shift=scanner.nextInt()
-    var str1=" "
+    var shift=scanner.nextInt()
+    var res:String=""
     if (str != null) {
-        for(i in str) {
-            val letter= if(i.isLetter())  i+shift else i
-            str1 += letter
+        for(j in str) {
+            res+=caesarCipher(j,shift)
         }
-     println(str1)
+        println(res)
     }
-    else
+    else{
         println("String should not be empty")
+    }
 }
+
+fun caesarCipher(j:Char,shift:Int):Char{
+    if (!j.isLetter()) {
+        return j
+    } else {
+        var d = j
+        var c = shift
+        while (c != 0) {
+            if (d != 'z' && d!='Z') {
+                d += 1
+                c -= 1
+            } else {
+                d = if(j.isLowerCase()) 'a' else 'A'
+                c -= 1
+            }
+        }
+        return d
+    }
+}
+
+
+
